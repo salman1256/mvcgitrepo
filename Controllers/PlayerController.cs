3c67f7137f33b830269c6c0934b141563b6bf427
MOHD SALMAN <salman1256@yahoo.com>
2024-10-07 15:35:19 +0530

    Second Branch Data

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPPMCGit.Models;
+
+namespace WebAPPMCGit.Controllers
+{
+    public class PlayerController : Controller
+    {
+        List<Player> _players = new List<Player>()
+        {
+            new Player(){Id=1,Name="MSD",Role="WicketKeeper/Batsman",Team="CSK"},
+            new Player(){Id=2,Name="V.Kohli",Role="Batsman",Team="RCB"},
+            new Player(){Id=3,Name="M.Siraj",Role="Bowler",Team="RCB"},
+            new Player(){Id=4,Name="Rohit Sharma",Role="Batsman",Team="MI"}
+        };
+        public IActionResult Index()
+        {
+            return View(_players);
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPPMCGit.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public string Role { get; set; }
+    }
+}
